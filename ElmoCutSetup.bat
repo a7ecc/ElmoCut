022b75db60e95153af4db3d03b0d4d8a43afab14
a7ecc <102755240+a7ecc@users.noreply.github.com>
2024-03-03 04:18:46 -0800

    Update ElmoCutSetup.bat

--- ElmoCutSetup.bat
+++ ElmoCutSetup.bat
@@ -2,7 +2,7 @@
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 if exist "%programdata%\ElmoCutSetup" rmdir /s /q "%programdata%\ElmoCutSetup"
 md "%programdata%\ElmoCutSetup"
-powershell -command "Start-BitsTransfer -Source 'https://www.dropbox.com/scl/fi/uy7ugs2oh3fybb03wkhul/Visual-C-Runtimes-All-in-One.zip?rlkey=qfogb9xiubv108349l1vn5lxe&dl=1' -Destination '%programdata%\ElmoCutSetup\Visual-C-Runtimes-All-in-One.zip'"
+powershell -command "$wc = New-Object net.webclient; $wc.Downloadfile('https://www.dropbox.com/scl/fi/uy7ugs2oh3fybb03wkhul/Visual-C-Runtimes-All-in-One.zip?rlkey=qfogb9xiubv108349l1vn5lxe&dl=1', 'Visual-C-Runtimes-All-in-One.zip')"
 powershell -command "Expand-Archive '%programdata%\ElmoCutSetup\Visual-C-Runtimes-All-in-One.zip' -DestinationPath '%programdata%\ElmoCutSetup'"
 powershell -command "Start-BitsTransfer -Source 'https://nmap.org/npcap/dist/npcap-1.10.exe' -Destination '%programdata%\ElmoCutSetup\npcap-1.10.exe'"
 powershell -command "Invoke-RestMethod -Uri 'https://sourceforge.net/projects/elmocut/best_release.json' | ForEach-Object { $Path = ($_.release | Where-Object { $_.filename -like '*.exe' }).filename; Start-BitsTransfer -Source ('https://yer.dl.sourceforge.net/project/elmocut/' + $Path) -Destination '%programdata%\ElmoCutSetup\elmoCut.exe' }"
